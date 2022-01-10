403daaf03ef58b954776f8dac886584f8e11fc16
Syed Amanat <cmd.callsyed@gmail.com>
2022-01-10 18:46:06 +0530

    ch

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	private static final String SECRET_KEY = "my_super_secret_key";
+	  private static final String SALT = "ssshhhhhhhhhhh!!!!";
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	//this will be your URL aka localhost:8080/encrypt?msg=helloworld
+	@RequestMapping(
+			value = "/encrypt", 
+			method=RequestMethod.GET
+			) 
+	   public String encrypt(@RequestParam(value="msg") String Msg) {
+		System.out.println(Msg);
+	      return encryptText(Msg);
+	   }
+	
+	//this will be your URL aka localhost:8080/decrypt
+	@RequestMapping(value="/decrypt", method=RequestMethod.GET)
+	public String decrypt(@RequestParam(value="msg") String Msg) {
+		System.out.println(Msg);
+		return decryptText(Msg);
+	}
+	
+	public static String encryptText(String strToEncrypt) {
+	    try {
+	      byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	      IvParameterSpec ivspec = new IvParameterSpec(iv);
+	 
+	      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+	      KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+	      SecretKey tmp = factory.generateSecret(spec);
+	      SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+	 
+	      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	      cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+	      return Base64.getEncoder()
+	          .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+	    } catch (Exception e) {
+	      System.out.println("Error while encrypting: " + e.toString());
+	    }
+	    return null;
+	  }
+	
+	
+	public static String decryptText(String strToDecrypt) {
+	    try {
+	      byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	      IvParameterSpec ivspec = new IvParameterSpec(iv);
+	 
+	      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+	      KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+	      SecretKey tmp = factory.generateSecret(spec);
+	      SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+	 
+	      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+	      cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+	      return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+	    } catch (Exception e) {
+	      System.out.println("Error while decrypting: " + e.toString());
+	    }
+	    return null;
+	  }
+}
